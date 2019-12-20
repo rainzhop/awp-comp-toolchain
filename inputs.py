b97758da48031419f67bdf8e7c88b54118e561ca
rainzhop <rainzhop@gmail.com>
2019-12-20 09:42:24 +0000

    增加自动划定200km×200km区域功能

--- inputs.py
+++ inputs.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 # 地震标识
 EQ_ID = '201906171455A'
 PIC_TITLE = '2014长宁M5.8地震模拟'
@@ -22,22 +24,21 @@ Mrp =  0.532*pow(10,25-7)
 Mtp =  2.900*pow(10,25-7)
 
 # crust1.0提取
-# 划定提取区域（左上角及右下角坐标）
+# 划定提取区域（左上角<lon1,lat1>及右下角<lon2,lat2>坐标）
 # 此处划定了一个200km*200km的区域
-# todo 此划定区域与格点尺寸和格点数有关，后续考虑改为自动划定范围
-lat1 = 29.23
-lon1 = 103.51
-lat2 = 27.43
-lon2 = 105.56
-#lat1 = HypocenterLati + 2 # 29.1
-#lon1 = HypocenterLong - 2 # 101.3
-#lat2 = HypocenterLati - 2 # 25.1
-#lon2 = HypocenterLong + 2 # 105.3
-# 经纬+深度方向的格点数
+# 此划定区域与格点尺寸和格点数有关,不可修改
+d = 100 # (km)
+r = 6371 # (km)
+delta_lat = 180*d / np.pi / r
+delta_lon = delta_lat / np.cos(HypocenterLati*np.pi/180)
+lat1 = HypocenterLati + delta_lat
+lon1 = HypocenterLong - delta_lon
+lat2 = HypocenterLati - delta_lat
+lon2 = HypocenterLong + delta_lon
 dh = 400 # 格点尺寸0.4km
-xlati = 500
-ylong = 500
-zdepth = 100
+xlati = 500 # 纬度方向格点数
+ylong = 500 # 经度方向格点数
+zdepth = 100 # 深度方向格点数
 
 Source_TMAX = 40 # Second
 
@@ -53,3 +54,4 @@ yt = ylong
 if __name__ == "__main__":
     env = "eq_id=%s X=%d Y=%d Z=%d DT=%f DH=%f TMAX=%f\n" % (EQ_ID, xlati, ylong, zdepth, dt, dh, Source_TMAX)
     print(env)
+    print("map range: %f, %f, %f, %f" %(lon1, lat1, lon2, lat2))
